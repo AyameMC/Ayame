117f6cdf72a0a3e16bda73898b422ecf2ec24a36
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-08-26 21:37:04 +0800

    注册Neoforge alt+Y组合键位

--- neoforge/src/main/java/org/ayamemc/ayame/neoforge/client/RegisterKeyMapping.java
+++ neoforge/src/main/java/org/ayamemc/ayame/neoforge/client/RegisterKeyMapping.java
@@ -6,6 +6,8 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.client.settings.KeyConflictContext;
+import net.neoforged.neoforge.client.settings.KeyModifier;
 import net.neoforged.neoforge.common.util.Lazy;
 import org.ayamemc.ayame.Ayame;
 import org.ayamemc.ayame.util.TranslatableName;
@@ -15,6 +17,8 @@ import org.lwjgl.glfw.GLFW;
 public class RegisterKeyMapping {
     public static final Lazy<KeyMapping> MODEL_SELECT_MENU = Lazy.of(() -> new KeyMapping(
             TranslatableName.SELECT_MODEL_MENU,
+            KeyConflictContext.IN_GAME,
+            KeyModifier.ALT,
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_Y,
             TranslatableName.MOD_KEY_MENU_NAME
