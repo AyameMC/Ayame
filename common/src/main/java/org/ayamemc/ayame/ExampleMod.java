8e7061c1197b3d9df5044877bbafaa2d22647c11
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-08-26 13:08:22 +0800

    init

---
+++ common/src/main/java/org/ayamemc/ayame/ExampleMod.java
@@ -0,0 +1,9 @@
+package org.ayamemc.ayame;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "ayame";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
