8dd61c778dbc313814bc4e54041a84e000f24548
crystalneko <minecrafthetun@outlook.com>
2024-08-27 22:27:36 +0800

    wtf

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@
 #
 
 chmod +x gradlew
-./gradlew runDatagen &      # 将命令放入后台运行
-pid=$!              # 获取该命令的PID
-sleep 60
-kill -9 $pid       # 60s后强制kill
+# ./gradlew runDatagen &      # 将命令放入后台运行
+# pid=$!              # 获取该命令的PID
+# sleep 60
+# kill -9 $pid       # 60s后强制kill
 
 ./gradlew build
 mkdir result
