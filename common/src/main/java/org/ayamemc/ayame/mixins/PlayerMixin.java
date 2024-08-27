b7be52f335c1d9ac88daaccc164ef19d8ff5d83a
crystalneko <minecrafthetun@outlook.com>
2024-08-27 11:36:19 +0800

    添加github action

--- common/src/main/java/org/ayamemc/ayame/mixins/PlayerMixin.java
+++ common/src/main/java/org/ayamemc/ayame/mixins/PlayerMixin.java
@@ -7,8 +7,10 @@ import org.spongepowered.asm.mixin.Unique;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.AnimatableManager;
+import software.bernie.geckolib.animation.AnimationController;
 import software.bernie.geckolib.animation.AnimationState;
 import software.bernie.geckolib.animation.PlayState;
+import software.bernie.geckolib.constant.DefaultAnimations;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 @Mixin(Player.class)
@@ -17,7 +19,21 @@ public abstract class PlayerMixin implements GeoEntity {
     private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        Player player = (Player)(Object)this;
+        // TODO 完善默认动画，支持自定义动画
+        controllers.add(new AnimationController<>(this, 40, state -> {
+            // 没有移动，则播放idle动画
+            if (!state.isMoving()){
+                state.getController().setAnimation(DefaultAnimations.IDLE);
+            }else if (state.isMoving()){
+//                if (this.getDeltaMovement().lengthSqr()< 0.01){
+//                    state.getController().setAnimation(DefaultAnimations.WALK);
+//                }else if (this.getDeltaMovement().lengthSqr() >= 0.01){
+//                    state.getController().setAnimation(DefaultAnimations.RUN);
+//                }
+                state.getController().setAnimation(DefaultAnimations.WALK);
+            }
+            return PlayState.CONTINUE;
+        }));
         // TODO 添加events
     }
 
