9e2c1439b209239ac0b18442bf332607600eb8a6
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-09-14 21:41:44 +0800

    更新版权

--- common/src/main/java/org/ayamemc/ayame/mixin/client/ClientItemInHandRendererMixin.java
+++ common/src/main/java/org/ayamemc/ayame/mixin/client/ClientItemInHandRendererMixin.java
@@ -1,27 +1,21 @@
 /*
- *      Custom player model mod. Powered by GeckoLib.
- *      Copyright (C) 2024  CrystalNeko, HappyRespawnanchor, pertaz(Icon Desiger)
+ *     Custom player model mod. Powered by GeckoLib.
+ *     Copyright (C) 2024  CrystalNeko, HappyRespawnanchor, pertaz(Icon Designer)
  *
- *      This file is part of Ayame.
+ *     This file is part of Ayame.
  *
- *     Ayame is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *     Ayame is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- *     Ayame is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *     Ayame is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
  *
- *     You should have received a copy of the GNU Lesser General Public License along with Ayame. If not, see <https://www.gnu.org/licenses/>.
- */
-
-/*
- *      Custom player model mod. Powered by GeckoLib.
- *      Copyright (C) 2024  CrystalNeko, HappyRespawnanchor, pertaz(Icon Desiger)
- *
- *      This file is part of Ayame.
- *
- *     Ayame is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- *
- *     Ayame is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public License along with Ayame. If not, see <https://www.gnu.org/licenses/>.
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with Ayame.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package org.ayamemc.ayame.mixin.client;
