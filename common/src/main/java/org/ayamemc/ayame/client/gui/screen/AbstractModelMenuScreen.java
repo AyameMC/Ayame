2d0fc8f8593afe619d0c6d3bc1321c3b3badcf47
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-10-04 20:01:21 +0800

    打开文件夹按钮添加

--- common/src/main/java/org/ayamemc/ayame/client/gui/screen/AbstractModelMenuScreen.java
+++ common/src/main/java/org/ayamemc/ayame/client/gui/screen/AbstractModelMenuScreen.java
@@ -23,8 +23,11 @@ package org.ayamemc.ayame.client.gui.screen;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.Util;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.components.ImageButton;
+import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.components.WidgetSprites;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
@@ -33,17 +36,19 @@ import org.ayamemc.ayame.Ayame;
 import org.ayamemc.ayame.client.gui.widget.BlurWidget;
 import org.jetbrains.annotations.Nullable;
 
+import java.nio.file.Path;
+
 @Environment(EnvType.CLIENT)
 public abstract class AbstractModelMenuScreen extends Screen {
     public static final ResourceLocation MENU_BACKGROUND_TEXTURE = withAyameNamespace("textures/gui/background.png");
     public static final ResourceLocation MENU_BACKGROUND_OUTLINE_TEXTURE = withAyameNamespace("textures/gui/background_outline.png");
     public static final ResourceLocation MENU_TOP_LAYER_TEXTURE = withAyameNamespace("textures/gui/top_layer.png");
     public static final ResourceLocation SETTINGS_TEXTURE = withAyameNamespace("textures/gui/settings.png");
+    private static final Path MODEL_DIR = Path.of("config/ayame/models/");
     private static final int BACKGROUND_TEXTURE_WIDTH = 410;
     private static final int BACKGROUND_TEXTURE_HEIGHT = 220;
     private static final int BUTTON_SIZE = 32;
-    private static final int LEFT_MARGIN = 0;
-    private static final int BOTTOM_MARGIN = 0;
+
     protected final Screen lastScreen;
 
     public AbstractModelMenuScreen(@Nullable Screen lastScreen) {
@@ -62,7 +67,18 @@ public abstract class AbstractModelMenuScreen extends Screen {
     protected void init() {
         BlurWidget blurredBackgroundWidget = new BlurWidget(getCenterX(BACKGROUND_TEXTURE_WIDTH), getCenterY(BACKGROUND_TEXTURE_HEIGHT), BACKGROUND_TEXTURE_WIDTH, BACKGROUND_TEXTURE_HEIGHT);
         this.addRenderableOnly(blurredBackgroundWidget);
+        EditBox searchBarEditBox = new EditBox(
+                this.font,
+                getComponentCenterX(208),
+                getComponentCenterY(20) - 90,
+                208,
+                20,
+                Component.translatable("ayame.widget.searchBarEditBox")
+        );
+        addRenderableWidget(searchBarEditBox);
+
     }
+
 
     /**
      * 渲染背景纹理
@@ -72,6 +88,7 @@ public abstract class AbstractModelMenuScreen extends Screen {
         RenderSystem.enableBlend();
         guiGraphics.blit(MENU_BACKGROUND_TEXTURE, getCenterX(BACKGROUND_TEXTURE_WIDTH), getCenterY(BACKGROUND_TEXTURE_HEIGHT), 0, 0, BACKGROUND_TEXTURE_WIDTH, BACKGROUND_TEXTURE_HEIGHT, BACKGROUND_TEXTURE_WIDTH, BACKGROUND_TEXTURE_HEIGHT);
         RenderSystem.disableBlend();
+
     }
 
     /**
@@ -90,24 +107,44 @@ public abstract class AbstractModelMenuScreen extends Screen {
                 withAyameNamespace("settings_disabled"), // 似乎不会被使用
                 withAyameNamespace("settings_enabled_focused")
         );
+        WidgetSprites opendirSprites = new WidgetSprites(
+                withAyameNamespace("opendir"),
+                withAyameNamespace("opendir"), // 似乎不会被使用
+                withAyameNamespace("opendir_enabled_focused")
+        );
         ImageButton settingsButton = new ImageButton(
-                getLeftAlignedX(BACKGROUND_TEXTURE_WIDTH, LEFT_MARGIN),
-                getBottomAlignedY(BACKGROUND_TEXTURE_HEIGHT, BUTTON_SIZE, BOTTOM_MARGIN),
+                getLeftAlignedX(BACKGROUND_TEXTURE_WIDTH, 0),
+                getBottomAlignedY(BACKGROUND_TEXTURE_HEIGHT, BUTTON_SIZE, 0),
                 BUTTON_SIZE,
                 BUTTON_SIZE,
                 settingSprites,
                 button -> {
-                    if (!(this instanceof SettingsScreen)) {
-                        minecraft.setScreen(new SettingsScreen(this));
-                    }
+                    Ayame.LOGGER.info("Setting button clicked.");
+                    minecraft.setScreen(new SettingsScreen(this));
                 },
                 Component.empty()
         );
-        if (this instanceof SettingsScreen) {
-            addRenderableOnly(settingsButton);
-        } else {
-            addRenderableWidget(settingsButton);
-        }
+        ImageButton opendirButton = new ImageButton(
+                getLeftAlignedX(BACKGROUND_TEXTURE_WIDTH, 0),
+                getBottomAlignedY(BACKGROUND_TEXTURE_HEIGHT, BUTTON_SIZE, 90),
+                BUTTON_SIZE,
+                BUTTON_SIZE,
+                opendirSprites,
+                button -> {
+                    Ayame.LOGGER.info("Opendir button clicked.");
+                    Util.getPlatform().openPath(MODEL_DIR);
+                },
+                Component.empty()
+        );
+        opendirButton.setTooltip(Tooltip.create(Component.translatable("ayame.button.opendir.tooltip")));
+
+        addRenderableWidget(settingsButton);
+        addRenderableWidget(opendirButton);
+//        if (this instanceof SettingsScreen) {
+//            addRenderableOnly(settingsButton);
+//        } else {
+//            addRenderableWidget(settingsButton);
+//        }
 
         Component text = Component.translatable(setTranslatableTitle());
 
@@ -167,6 +204,20 @@ public abstract class AbstractModelMenuScreen extends Screen {
     protected int getCenteredStringX(Component text) {
         int textWidth = this.font.width(text);
         return (this.width - textWidth) / 2;
+    }
+
+    /**
+     * 获取与贴图无关的组件在屏幕中心的 X 坐标
+     */
+    protected int getComponentCenterX(int componentWidth) {
+        return (this.width - componentWidth) / 2;
+    }
+
+    /**
+     * 获取与贴图无关的组件在屏幕中心的 Y 坐标
+     */
+    protected int getComponentCenterY(int componentHeight) {
+        return (this.height - componentHeight) / 2;
     }
 
     @Override
