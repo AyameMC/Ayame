66dfb3599b85057e68ca40388d4c1912541f7767
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2025-01-23 21:12:24 +0800

    继续完善

--- common/src/main/java/org/ayamemc/ayame/mixin/AnimationControllerMixin.java
+++ common/src/main/java/org/ayamemc/ayame/mixin/AnimationControllerMixin.java
@@ -26,7 +26,9 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.Direction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import software.bernie.geckolib.animatable.GeoAnimatable;
 import software.bernie.geckolib.animation.AnimationController;
 import software.bernie.geckolib.animation.keyframe.*;
 import software.bernie.geckolib.animation.state.BoneSnapshot;
@@ -35,22 +37,65 @@ import software.bernie.geckolib.loading.math.MathValue;
 import java.util.List;
 
 @Mixin(AnimationController.class)
-public abstract class AnimationControllerMixin {
+public abstract class AnimationControllerMixin<T extends GeoAnimatable> {
     @Shadow
     protected abstract AnimationPoint getAnimationPointAtTick(List<Keyframe<MathValue>> frames, double tick, boolean isRotation, Direction.Axis axis);
-
+    
     @WrapOperation(
             method = "process",
             at = @At(value = "INVOKE", target = "Lsoftware/bernie/geckolib/animation/keyframe/BoneAnimationQueue;addNextRotation(Lsoftware/bernie/geckolib/animation/keyframe/Keyframe;DDLsoftware/bernie/geckolib/animation/state/BoneSnapshot;Lsoftware/bernie/geckolib/animation/state/BoneSnapshot;Lsoftware/bernie/geckolib/animation/keyframe/AnimationPoint;Lsoftware/bernie/geckolib/animation/keyframe/AnimationPoint;Lsoftware/bernie/geckolib/animation/keyframe/AnimationPoint;)V"),
             remap = false
     )
     private void process(BoneAnimationQueue instance, Keyframe<?> keyFrame, double lerpedTick, double transitionLength, BoneSnapshot startSnapshot, BoneSnapshot initialSnapshot, AnimationPoint nextXPoint, AnimationPoint nextYPoint, AnimationPoint nextZPoint, Operation<Void> original,
-                         @Local(name = "rotationKeyFrames") KeyframeStack<Keyframe<MathValue>> rotationKeyFrames, @Local BoneAnimation boneAnimation
-    ) {
+                         @Local(name = "rotationKeyFrames") KeyframeStack<Keyframe<MathValue>> rotationKeyFrames, @Local BoneAnimation boneAnimation) {
+        final List<Keyframe<MathValue>> xKeyframes = rotationKeyFrames.xKeyframes();
+        final List<Keyframe<MathValue>> yKeyframes = rotationKeyFrames.yKeyframes();
+        final List<Keyframe<MathValue>> zKeyframes = rotationKeyFrames.zKeyframes();
+
+        for (int i = 0; i < xKeyframes.size(); i++) {
+            final Keyframe<MathValue> xKeyframe = xKeyframes.get(i);
+            final Keyframe<MathValue> yKeyframe = yKeyframes.get(i);
+            final Keyframe<MathValue> zKeyframe = zKeyframes.get(i);
+            final Keyframe<MathValue> xKeyframeCopy = new Keyframe<>(
+                    xKeyframe.length(),
+                    boxingToMathValue(xKeyframe.startValue().get() + 1),
+                    xKeyframe.endValue(),
+                    xKeyframe.easingType(),
+                    xKeyframe.easingArgs()
+            );
+            final Keyframe<MathValue> yKeyframeCopy = new Keyframe<>(
+                    yKeyframe.length(),
+                    boxingToMathValue(yKeyframe.startValue().get() + 1),
+                    yKeyframe.endValue(),
+                    yKeyframe.easingType(),
+                    yKeyframe.easingArgs()
+            );
+            final Keyframe<MathValue> zKeyframeCopy = new Keyframe<>(
+                    zKeyframe.length(),
+                    boxingToMathValue(zKeyframe.startValue().get() + 1),
+                    zKeyframe.endValue(),
+                    zKeyframe.easingType(),
+                    zKeyframe.easingArgs()
+            );
+
+            xKeyframes.set(i, xKeyframeCopy);
+            yKeyframes.set(i, yKeyframeCopy);
+            zKeyframes.set(i, zKeyframeCopy);
+        }
+
         original.call(instance, keyFrame, lerpedTick, transitionLength, startSnapshot, initialSnapshot,
-                getAnimationPointAtTick(rotationKeyFrames.yKeyframes(), 0, true, Direction.Axis.X),
+                getAnimationPointAtTick(rotationKeyFrames.xKeyframes(), 0, true, Direction.Axis.X),
                 getAnimationPointAtTick(rotationKeyFrames.yKeyframes(), 0, true, Direction.Axis.Y),
-                getAnimationPointAtTick(rotationKeyFrames.zKeyframes(), 0, true, Direction.Axis.Z)
-        );
+                getAnimationPointAtTick(rotationKeyFrames.zKeyframes(), 0, true, Direction.Axis.Z));
+    }
+
+    @Unique
+    private static double unboxToDouble(MathValue value) {
+        return value.get();
+    }
+
+    @Unique
+    private static MathValue boxingToMathValue(double value) {
+        return () -> value;
     }
 }
