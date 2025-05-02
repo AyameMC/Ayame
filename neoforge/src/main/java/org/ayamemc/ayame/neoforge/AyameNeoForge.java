fca8e0fb7fd1cca28c159d649d42e0e4de3e8f04
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2025-05-02 12:03:13 +0800

    注释neo声音

--- neoforge/src/main/java/org/ayamemc/ayame/neoforge/AyameNeoForge.java
+++ neoforge/src/main/java/org/ayamemc/ayame/neoforge/AyameNeoForge.java
@@ -31,6 +31,6 @@ public final class AyameNeoForge {
         // AyameNeoForgeSounds.SOUND_EVENTS.register(modBus);
         // Run our common setup.
         // TODO：解决注册过晚问题
-        Ayame.init(ModLoader.NEOFORGE, AyameNeoForgeSounds.AYAME_SOUND.get());
+        Ayame.init(ModLoader.NEOFORGE,null);
     }
 }
